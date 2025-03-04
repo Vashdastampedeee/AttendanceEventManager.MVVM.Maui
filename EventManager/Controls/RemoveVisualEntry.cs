2b8dd83c81dcf182e2702d9a84a466064f613b8c
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-04 08:29:15 +0800

    Add: Controls>RemoveVisualEntry.cs - for removing visual underline in the entry

---
+++ EventManager/Controls/RemoveVisualEntry.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+namespace EventManager.Controls
+{
+    public class RemoveVisualEntry : Entry
+    {
+        protected override void OnHandlerChanged()
+        {
+            base.OnHandlerChanged();
+            RemoveVisual();
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == nameof(BackgroundColor))
+            {
+                RemoveVisual();
+            }
+        }
+
+        private void RemoveVisual()
+        {
+#if ANDROID
+        if (Handler is IEntryHandler entryHandler) 
+        {
+            if(BackgroundColor == null)
+            {
+             entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());    
+            }
+            else
+            {
+             entryHandler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(BackgroundColor.ToPlatform());    
+            }
+                        
+        }
+#endif
+        }
+    }
+}
