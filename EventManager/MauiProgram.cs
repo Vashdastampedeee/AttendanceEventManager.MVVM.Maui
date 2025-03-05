9e0e537d6a379896fee248737a98acaf8ce9ba85
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-05 14:15:36 +0800

    Modify: MauiProgram - register services of beep service

--- EventManager/MauiProgram.cs
+++ EventManager/MauiProgram.cs
@@ -24,6 +24,7 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 		builder.Services.AddSingleton<DatabaseService>();
+		builder.Services.AddSingleton<BeepService>();
 		builder.Services.AddSingleton<IndexViewModel>();
 
 		return builder.Build();
