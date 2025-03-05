79823ded18c4a68d530d01e34d8ac240f5b29cd8
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-05 14:13:33 +0800

    Add: BeepService -added to services for sfx in barcode id number scanning

---
+++ EventManager/Services/BeepService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Plugin.Maui.Audio;
+
+namespace EventManager.Services
+{
+    public class BeepService
+    {
+        private IAudioPlayer player;
+
+        public async Task InitBeep()
+        {
+            if (player == null)
+            {
+                var stream = await FileSystem.OpenAppPackageFileAsync("barcodeSfx.mp3");
+                player = AudioManager.Current.CreatePlayer(stream);
+            }
+        }
+
+        public void PlayBeep()
+        {
+            player.Play();
+        }
+
+    }
+}
