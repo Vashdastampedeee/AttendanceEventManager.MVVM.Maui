c974bc9d0392f9af9227e34dbe07bb4335d6e27b
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-05 13:10:25 +0800

    Add: ImageHelper - added a class for converting byte to image

---
+++ EventManager/Utilities/ImageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Utilities
+{
+    public static class ImageHelper
+    {
+        public static ImageSource ConvertBytesToImage(byte[] imageSource)
+        {
+            if(imageSource == null || imageSource.Length == 0)
+            {
+                return "blank_id.pmg";
+            }
+
+            return ImageSource.FromStream(() => new MemoryStream(imageSource));
+
+        }
+    }
+}
