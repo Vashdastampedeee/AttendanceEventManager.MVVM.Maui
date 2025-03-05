9f8b067f9e2b7b8b43b97050484434d1bc6f16ec
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-05 13:06:32 +0800

    Add: FocusEntry - added a control for focusing on entry barcode in page appearing in home page

---
+++ EventManager/Controls/FocusEntry.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Controls
+{
+    public class FocusEntry : Behavior<Entry> 
+    {
+        public static readonly BindableProperty IsFocusedProperty =
+            BindableProperty.Create(
+                nameof(IsFocused),
+                typeof(bool),
+                typeof(FocusEntry),
+                false,
+                propertyChanged: OnIsFocusedChanged);
+
+        public bool IsFocused
+        {
+            get => (bool)GetValue(IsFocusedProperty);
+            set => SetValue(IsFocusedProperty, value);
+        }
+
+        private static void OnIsFocusedChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is FocusEntry behavior && behavior.AssociatedObject is Entry entry)
+            {
+                Debug.WriteLine($"[FocusEntry] IsFocused changed: {newValue}");
+                if ((bool)newValue)
+                {
+                    Debug.WriteLine("[FocusEntry] Triggering focus from IsFocused change");
+                    behavior.AssociatedObject?.Focus(); 
+                }
+            }
+        }
+
+        private Entry AssociatedObject;
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            base.OnAttachedTo(entry);
+            AssociatedObject = entry;
+            Debug.WriteLine("[FocusEntry] Behavior attached to Entry");
+
+            Debug.WriteLine($"[FocusEntry] IsFocused value on attach: {IsFocused}");
+
+            if (IsFocused)
+            {
+                Debug.WriteLine("[FocusEntry] Triggering focus on attach");
+                entry.Focus();
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            base.OnDetachingFrom(entry);
+            AssociatedObject = null;
+        }
+    }
+}
