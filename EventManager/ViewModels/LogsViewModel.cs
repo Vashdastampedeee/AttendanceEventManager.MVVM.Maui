039298e7ddd64176482cd094ed797e14b5fca35e
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-06 10:04:18 +0800

    Add: LogsViewModel - added in ViewModels folder

---
+++ EventManager/ViewModels/LogsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using EventManager.Services;
+
+namespace EventManager.ViewModels
+{
+    public partial class LogsViewModel : ObservableObject
+    {
+        private readonly DatabaseService databaseService;
+        public LogsViewModel(DatabaseService databaseServiceInjection) 
+        {
+            databaseService = databaseServiceInjection;
+        }
+    }
+}
