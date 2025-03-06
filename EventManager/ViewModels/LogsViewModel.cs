736f4c5ea2682cec31e0d7aa326add43796e1321
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-06 14:46:06 +0800

    Modify: LogsView and LogsViewModel - added collection view of logs of employee with activity indicator

--- EventManager/ViewModels/LogsViewModel.cs
+++ EventManager/ViewModels/LogsViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EventManager.Models;
 using EventManager.Services;
 
 namespace EventManager.ViewModels
@@ -11,9 +15,42 @@ namespace EventManager.ViewModels
     public partial class LogsViewModel : ObservableObject
     {
         private readonly DatabaseService databaseService;
+        
+        [ObservableProperty]
+        private ObservableCollection<AttendanceLog> attendanceLogs = new();
+
+        [ObservableProperty]
+        private bool isBusyIndicator;
+
+        [ObservableProperty]
+        private bool isEnabled;
+
         public LogsViewModel(DatabaseService databaseServiceInjection) 
         {
             databaseService = databaseServiceInjection;
+            LoadAttendanceLogs();
         }
+
+        [RelayCommand]
+        public async Task LoadAttendanceLogs()
+        {
+            IsEnabled = false;
+            IsBusyIndicator = true;
+            var logs = await databaseService.GetAllAttendanceLogs();
+          
+
+            if (logs != null)
+            {
+                AttendanceLogs.Clear();
+                await Task.Delay(1000);
+                foreach (var log in logs)
+                {
+                    AttendanceLogs.Add(log);
+                }
+            }
+            IsEnabled = true;
+            IsBusyIndicator = false;
+        }
+
     }
 }
--- EventManager/Views/Logs.xaml.cs
+++ EventManager/Views/Logs.xaml.cs
@@ -1,9 +1,12 @@
+using EventManager.ViewModels;
+
 namespace EventManager.Views;
 
 public partial class Logs : ContentPage
 {
-	public Logs()
+	public Logs(LogsViewModel logsViewModel)
 	{
 		InitializeComponent();
+		BindingContext = logsViewModel;
 	}
 }
