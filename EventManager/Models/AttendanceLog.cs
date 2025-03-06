42d50f29ac2c859f6e3505905df91ddc463006ac
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-06 08:24:14 +0800

    Add: AttendanceLog - added in the models folder

---
+++ EventManager/Models/AttendanceLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace EventManager.Models
+{
+    [Table("attendancelog")]
+    public class AttendanceLog
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string IdNumber { get; set; }
+        public string Name { get; set; }
+        public string BusinessUnit { get; set; }
+        public string Status { get; set; }
+        public string Timestamp { get; set; }
+
+    }
+}
