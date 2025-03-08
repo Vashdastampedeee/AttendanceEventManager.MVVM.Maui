fcf9f309a1bf48d1ec17fbed8b6799af88d866a0
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-08 08:21:28 +0800

    Add: Event Model and EventViewModel - created c# files

---
+++ EventManager/Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace EventManager.Models
+{
+    [Table("event")]
+    public class Event
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public string EventCategory { get; set; }
+        public byte[] EventImage { get; set; }
+        public bool isSelected { get; set; }
+        public string EventDate { get; set; }
+        public string EventFromTime { get; set; }
+        public string EventToTime { get; set; }
+    }
+}
