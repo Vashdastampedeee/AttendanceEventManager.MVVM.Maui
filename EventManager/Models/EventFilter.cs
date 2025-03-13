b5fa2534179af1947c274abc7b56c3b62e8acc26
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-13 12:11:48 +0800

    Modify: EventFilter - created model for event filter

---
+++ EventManager/Models/EventFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Models
+{
+    public class EventFilter
+    {
+        public string Category { get; set; }
+        public bool Order {  get; set; }
+    }
+}
