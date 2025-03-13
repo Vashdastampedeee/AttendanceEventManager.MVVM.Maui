156ee4fd4dbde3aef3ae24748063a42988197515
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-14 07:32:25 +0800

    Modify: Android/MainActivity - added app permission for external and internal storage for lower OS of android devices

--- EventManager/Platforms/Android/MainActivity.cs
+++ EventManager/Platforms/Android/MainActivity.cs
@@ -1,10 +1,34 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 
 namespace EventManager;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    //LOWER VERSION OF ANDROID DEVICES ADD APP PERMISSION FOR WRITING OR READING STORAGE
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.M && Build.VERSION.SdkInt < BuildVersionCodes.Q)
+        {
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new string[]
+                {
+                        Manifest.Permission.WriteExternalStorage,
+                        Manifest.Permission.ReadExternalStorage
+                }, 1);
+            }
+        }
+    }
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+    {
+        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
