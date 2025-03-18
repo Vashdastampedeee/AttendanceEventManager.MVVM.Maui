7a951361287f738576ceedb9f77468c8600a02f8
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-19 07:44:40 +0800

    Modify: Appshell - register route of TotalScannedData modal

--- EventManager/AppShell.xaml.cs
+++ EventManager/AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace EventManager
+﻿using EventManager.Views.Modals;
+
+namespace EventManager
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute(nameof(TotalScannedData), typeof(TotalScannedData));
         }
     }
 }
