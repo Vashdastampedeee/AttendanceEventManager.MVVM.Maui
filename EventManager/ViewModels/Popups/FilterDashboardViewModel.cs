84356b8f175000faef53a1043dbe48770d0714c9
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-18 14:28:58 +0800

    Added: FilterDashboardViewModel - with working logics and functions

---
+++ EventManager/ViewModels/Popups/FilterDashboardViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Mopups.Services;
+
+namespace EventManager.ViewModels.Popups
+{
+    public partial class FilterDashboardViewModel : ObservableObject
+    {
+        private readonly DashboardViewModel dashboardViewModel;
+
+        [ObservableProperty]
+        private string selectedBusinessUnit;
+
+        public List<string> BusinessUnitOption { get; } = new() { "ALL", "BAG", "BBG WAREHOUSE", "HLB", "JLINE", "RAWLINGS", "SUPPORT GROUP", "TRI-VIET" };
+        public FilterDashboardViewModel(DashboardViewModel dashboardViewModel) 
+        {
+            this.dashboardViewModel = dashboardViewModel;
+            SelectedBusinessUnit = dashboardViewModel.SelectedBusinessUnit; 
+        }
+
+        [RelayCommand]
+        public async Task ApplyFilter()
+        {
+            await MopupService.Instance.PopAsync();
+            await dashboardViewModel.ApplyFilter(SelectedBusinessUnit);
+        }
+
+
+    }
+}
