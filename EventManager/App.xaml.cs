a4c96ee3009fa864ddc147e7c0b36a0c8a8a8ca7
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-21 07:41:33 +0800

    Modify: App - register community license key of syncfusion

--- EventManager/App.xaml.cs
+++ EventManager/App.xaml.cs
@@ -5,6 +5,8 @@
         public App()
         {
             InitializeComponent();
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVFwWmFZfVtgfF9GYlZTTWY/P1ZhSXxWdkZiUH1ecHZXT2hUUEY=");
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
