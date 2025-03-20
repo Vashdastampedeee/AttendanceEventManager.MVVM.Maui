36b4bd1b8ef2b27e12cc6d491e7d94e1b4ecd0db
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-21 07:42:57 +0800

    Add: AttendanceByBU & AttendanceCategory - added model that will be used to bind in syncfusion pie and cartesian charts

---
+++ EventManager/Models/AttendanceByBU.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Models
+{
+    public class AttendanceByBU
+    {
+        public string BusinessUnit { get; set; }
+        public double Count { get; set; }
+    }
+}
---
+++ EventManager/Models/AttendanceCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Models
+{
+    public class AttendanceCategory
+    {
+        public string Category { get; set; }
+        public double Count { get; set; }
+    }
+}
