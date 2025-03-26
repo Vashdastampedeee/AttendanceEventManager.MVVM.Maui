18bcc6c6dadf1810f135881d2ca602fb8d756d2a
=Ian Vash Ryzen Lelis <=ianlelis62@gmail.com>
2025-03-26 10:54:16 +0800

    Add: ScanStatusHelper - added utilities for scan status string and frame color

---
+++ EventManager/Utilities/ScanStatusHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Utilities
+{
+    public static class ScanStatusHelper
+    {
+        public static class StatusString
+        {
+            public const string AlreadyScanned = "ALREADY SCANNED";
+            public const string NotFound = "NOT FOUND";
+            public const string Success = "SUCCESS";
+        }
+        public static class FrameColor
+        {
+            public static readonly Color Error = Color.FromArgb("#D50000");
+            public static readonly Color Success = Color.FromArgb("#00C853");
+            public static readonly Color Default = Color.FromArgb("#009AFE");
+        }
+    }
+}
