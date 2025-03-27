cbf46c5f84259aee6a05cabe8359e7d1e7b8c4fd
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-27 14:50:45 +0800

    Modify: OptionViewModel - removed unnecessary library

--- EventManager/ViewModels/OptionViewModel.cs
+++ EventManager/ViewModels/OptionViewModel.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
-using Kotlin.Properties;
 using Syncfusion.Maui.Buttons;
 
 namespace EventManager.ViewModels
