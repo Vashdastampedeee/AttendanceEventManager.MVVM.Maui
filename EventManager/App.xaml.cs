f957308fde3d93c626276dbb8616709e26161164
Ian Vash Ryzen Lelis <Ianlelis62@gmail.com>
2025-03-27 12:42:02 +0800

    Modify: App - added method of lifecycle OnStart() for loading saved theme values

--- EventManager/App.xaml.cs
+++ EventManager/App.xaml.cs
@@ -5,13 +5,30 @@
         public App()
         {
             InitializeComponent();
-
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mzc2NzExM0AzMjM4MmUzMDJlMzBvZ2Fpcyt3RzJXSHFmeTFIajNrVlhQUXBrdE11UXlFaEhRcHlCUWgzaTVzPQ==");
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            ApplySavedTheme();
+        }
+
+        private void ApplySavedTheme()
+        {
+            int savedTheme = Preferences.Get("AppTheme", 0);
+
+            UserAppTheme = savedTheme switch
+            {
+                1 => AppTheme.Light,
+                2 => AppTheme.Dark,
+                _ => AppTheme.Unspecified
+            };
         }
     }
 }
